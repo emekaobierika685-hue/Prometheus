ab237ad979b0184a75ba40a96162b0003f35af0a
emekaobierika685-hue <emekaobierika685@gmail.com>
2026-05-09 21:20:37 -0400

    Update deobf_cli.lua

--- deobf_cli.lua
+++ deobf_cli.lua
@@ -9,45 +9,5 @@ local function script_path()
     return str:match("(.*[/%\\])") or ""
 end
 package.path = script_path() .. "?.lua;" .. package.path
-package.path = script_path() .. "src/?.lua;" .. script_path() .. "src/?/init.lua;" .. package.path
 
-local deobf = require("prometheus.deobf")
-
-local inputFile  = arg[1]
-local outputFile = arg[2]
-
-if not inputFile then
-    print("Usage: lua deobf_cli.lua input.lua [output.lua]")
-    os.exit(1)
-end
-
--- Read input file
-local f = io.open(inputFile, "r")
-if not f then
-    print("Error: cannot open '" .. inputFile .. "'")
-    os.exit(1)
-end
-local source = f:read("*a")
-f:close()
-
--- Run deobfuscator
-local result, err = deobf:run(source, { verbose = true })
-
-if not result then
-    print("Deobfuscation failed: " .. tostring(err))
-    os.exit(1)
-end
-
--- Write or print output
-if outputFile then
-    local out = io.open(outputFile, "w")
-    if not out then
-        print("Error: cannot write to '" .. outputFile .. "'")
-        os.exit(1)
-    end
-    out:write(result)
-    out:close()
-    print("Written to: " .. outputFile)
-else
-    print(result)
-end
+require("src.deobf_cli")
